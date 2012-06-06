d98ff0d78f72be1857c9ff26ef62a210d8ee58c7
tidjean <tidjean@gmail.com>
2012-06-06 17:25:10 +0800

    no remember basket after login

--- checkout.php
+++ checkout.php
@@ -91,7 +91,7 @@ if (isset($HTTP_GET_VARS['action']) && ($HTTP_GET_VARS['action'] == 'process')) 
 			tep_db_query("update " . TABLE_CUSTOMERS_INFO . " set customers_info_date_of_last_logon = now(), customers_info_number_of_logons = customers_info_number_of_logons+1 where customers_info_id = '" . (int)$customer_id . "'");
 
 			// restore cart contents
-			$cart->restore_contents();
+			//$cart->restore_contents();
 
 			if (sizeof($navigation->snapshot) > 0) {
 				$origin_href = tep_href_link($navigation->snapshot['page'], tep_array_to_string($navigation->snapshot['get'], array(tep_session_name())), $navigation->snapshot['mode']);
