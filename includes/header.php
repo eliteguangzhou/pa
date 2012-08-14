7c86a1d44845152f6dd726d8077c013ed8986a9c
Tidjean <tidjean@gmail.com>
2012-08-14 18:36:10 +0200

    premier jour

--- includes/header.php
+++ includes/header.php
@@ -66,7 +66,7 @@
     $res = tep_db_fetch_array($res);
     $res = $res['new'];
     if ($res > 0){
-      $customer_name .= '<br/><a class="banner_login" href="'.FILENAME_MY_TICKET.'">'.NEW_MESSAGES.' ('.$res.')</a>'.$_SESSION['customer_id'];
+      $customer_name .= '<br/><a class="banner_login" href="'.FILENAME_MY_TICKET.'">'.NEW_MESSAGES.' ('.$res.')</a>';
     }
     else {
      $customer_name .= '<br/><a class="banner_login" href="">'.NO_MESSAGE.'</a>';
