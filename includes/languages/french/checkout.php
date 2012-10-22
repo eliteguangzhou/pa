ce31b26474fc37ec204f395e57b8c6ad013fcbd1
tidjean <tidjean@gmail.com>
2012-10-22 13:33:14 +0200

    fp

--- includes/languages/french/checkout.php
+++ includes/languages/french/checkout.php
@@ -13,7 +13,7 @@ define(TITLE_PAYMENT_METHOD,'Methode de paiement 100% securis&eacute;:');
 define(TEXT_ENTER_PAYMENT_INFORMATION,'Paypal');
 define(TEXT_EDIT,'Editer ma commande');
 define(SUB_TITLE_SUB_TOTAL,'Sous-total');
-define(SUB_TITLE_FRAIS_PORT,'Frais de port (france)');
+define(SUB_TITLE_FRAIS_PORT,'Frais de port (France)');
 define('SUB_TITLE_FRAIS_PORT_SUP','Suppl&eacute;ment (hors France)');
 define(SUB_TITLE_TOTAL,'Total');
 define(FREE_SHIPPING_COST,'Gratuit');
